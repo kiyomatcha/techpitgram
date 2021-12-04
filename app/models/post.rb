718c47eaa50ed204bbaa194fb6ee6c257de8473a
KIYOMATCHA <athlete.kyogo@gmail.com>
2021-12-04 22:39:40 +0900

    finish ch8-1

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :photos, dependent: :destroy
-  has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
-
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
   accepts_nested_attributes_for :photos
 
-  # ==========ここから追加する==========
-  def liked_by(user)
-    # user_idとpost_idが一致するlikeを検索する
-    Like.find_by(user_id: user.id, post_id: id)
+  def liked_by(current_user)
+    Like.find_by(user_id: current_user.id, post_id: id)
   end
-  # ==========ここまで追加する==========
-
 end
